c8c7f38dbcc03fe5a00cccb1f528695b2e8d77b0
Georgi <g_garenov@outlook.com>
2025-07-08 17:53:09 +0300

    The new pages are added in the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 // import { AuthLayout } from "./components/auth/layout";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
