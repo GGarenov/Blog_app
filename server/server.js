4c9131a3a082e4dc2a3da560ee4488b5e48b456f
Georgi <g_garenov@outlook.com>
2025-07-08 13:08:18 +0300

    Auth routes is implemented in the server.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRouter = require("./routes/auth/auth-routes");
 
 dotenv.config();
 
@@ -14,5 +15,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
